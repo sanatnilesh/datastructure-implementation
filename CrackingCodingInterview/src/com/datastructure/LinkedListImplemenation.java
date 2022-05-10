54eeea76518d437c168b91252cd319ddc3e9e668
Sanatkumar Dhobi <sanat541997@gmail.com>
2022-05-10 07:59:08 -0400

    Linkedlist

--- CrackingCodingInterview/src/com/datastructure/LinkedListImplemenation.java
+++ CrackingCodingInterview/src/com/datastructure/LinkedListImplemenation.java
@@ -1,7 +1,14 @@
 package com.datastructure;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 
+/**
+ * remove duplicate from the given linkedlist.
+ * 
+ * @author Sanatkumar
+ *
+ */
 public class LinkedListImplemenation {
 	public static void main(String[] args) {
 		LinkedList<Integer> linkedList = new LinkedList<>();
@@ -10,7 +17,30 @@ public class LinkedListImplemenation {
 		linkedList.add(5);
 		linkedList.add(4);
 		linkedList.add(1);
+
+		removeDuplicate(linkedList);
 		
-		
+		//use Node class
+		Node node = new Node(2);
+		node.appendToEnd(3);
+		node.appendToEnd(1);
+		node.appendToEnd(5);
+		node.appendToEnd(11);
+		node.appendAtStart(0);
+		node.appendAtStart(2);
+		node.print();
+	}
+
+	private static void removeDuplicate(LinkedList<Integer> linkedList) {
+		HashSet<Integer> hashSet = new HashSet<>();
+
+		for (Integer integer : linkedList) {
+			if (hashSet.contains(integer)) {
+				linkedList.remove(integer);
+			} else {
+				hashSet.add(integer);
+			}
+		}
+		System.out.println(hashSet);
 	}
 }
---
+++ CrackingCodingInterview/src/com/datastructure/Node.java
@@ -0,0 +1,47 @@
+package com.datastructure;
+
+public class Node {
+	Node next;
+	int data;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public Node head = null;
+
+	public void appendToEnd(int d) {
+		Node newNode = new Node(d);
+		Node temp = head;
+		if (head == null) {
+			head = newNode;
+		}else {
+			while(temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+		}
+		
+	}
+
+	public void appendAtStart(int d) {
+		Node newNode = new Node(d);
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node temp = head;
+			head = newNode;
+			head.next = temp;
+		}
+	}
+
+	public void print() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + "->");
+			temp = temp.next;
+		}
+	}
+
+}
